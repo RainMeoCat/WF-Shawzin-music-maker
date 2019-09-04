d4f302060f7a7ce37c728d6cb697ab9487738ce4
unknown <akimotoming@gmail.com>
2019-09-04 22:42:02 +0800

    L2d dialog force customized and add a hellsongcreator

--- src/main.js
+++ src/main.js
@@ -1,40 +1,66 @@
 
 
-function copySong(){
-    const input = document.querySelector('#SongString');
-    input.select();
-    if (document.execCommand('copy')) {
-        document.execCommand('copy');
-        console.log('複製成功');
-        $("[data-toggle='popover']").popover("toggle");
-        setTimeout(function () {
-            $('.popover').popover('hide');
-        }, 2000);
-}}
-$(function () { $("[data-toggle='tooltip']").tooltip(); });
-var dialog={
-    "dialog": [
-      "哈囉!",
-      "編曲編得如何？我可以聽嗎？",
-      "記得休息，休息是為了農更多的赤毒",
-      "喜歡的話可以去我的Github給個星星~",
-      "記得去我的Youtube頻道訂閱喔!"
-    ]
+function copySong() {
+  const input = document.querySelector('#SongString');
+  input.select();
+  if (document.execCommand('copy')) {
+    document.execCommand('copy');
+    console.log('複製成功');
+    $("[data-toggle='SSpopover']").popover("toggle");
+    setTimeout(function () {
+      $("[data-toggle='SSpopover']").popover('hide');
+    }, 2000);
   }
-  console.log(Math.floor(Math.random()*6))
+}
+$(function () { $("[data-toggle='tooltip']").tooltip(); });
+var dialog = {
+  "dialog": [
+    "喵喵喵?",
+    "編曲編得如何？我可以聽嗎？",
+    "記得休息，休息是為了農更多的赤毒",
+    "最強的DPS戰甲就是TITANIA了，不接受反駁◑ω◐",
+    "其實雨喵我根本沒有音樂底子，還要問我學鋼琴的弟弟ˊwˋ",
+    "我的WARFRAME ID:RainMeoCat 記得標上註解喔!",
+    "最近好忙呀，沒有時間農(´༎ຶ۝༎ຶ)",
+    "其實我也是很想彈一首Through The Fire And Flames呀｡ﾟヽ(ﾟ´Д`)ﾉﾟ｡",
+    "雨喵家的貓貓叫蛋蛋，是隻美國短毛貓(*´∀`)~♥",
+    "其實我的每隻戰甲都有買TennoGEN喔d(`･∀･)b"
+  ]
+}
+console.log(Math.floor(Math.random() * 6))
+console.log(Object.keys(dialog.dialog).length)
 L2Dwidget.init({
-    dialog: {
-      // 开启对话框
-      enable: true,
-      script: {
-        // 每空闲 10 秒钟，显示一条一言
-        'every idle 20s': '$hitokoto$',
-        // 当触摸到星星图案
-        'hover .star': '星星在天上而你在我心里 (*/ω＼*)',
-        // 当触摸到角色身体
-        'tap body': '哎呀！别碰我！',
-        // 当触摸到角色头部
-        'tap face': '人家已经不是小孩子了！'
-      }
+  dialog: {
+    enable: true,
+    script: {
+      'every idle 10s': '$hitokoto$',
+      'hover .youtube': '我的Youtube頻道，記得訂閱喔!',
+      'hover .bilibili': '我的Bilibili頻道，記得三連加關注~',
+      'hover .github': '喜歡的話可以去我的Github給個星星~',
+
     }
-  });
+  }
+});
+function create_hell_song() {
+  const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  const notes = "BRhJCSKEUkiM";
+  const totalValues = Math.pow(data.length,2);
+  let output = String(Math.floor(Math.random() * 7) + 1);
+  for(let i = 0;i<100;i++) {
+      output += notes[Math.floor(Math.random() * notes.length)];
+      const c1 = data[Math.floor(i / data.length)];
+      const c2 = data[i % data.length];
+      output += c1 + c2;
+  }
+  document.getElementById("HellSongString").value=output
+  const input = document.querySelector('#HellSongString');
+  input.select();
+  if (document.execCommand('copy')) {
+    document.execCommand('copy');
+    console.log('複製成功');
+    $("[data-toggle='HSSpopover']").popover("toggle");
+    setTimeout(function () {
+      $("[data-toggle='HSSpopover']").popover('hide');
+    }, 2000);
+  }
+}
