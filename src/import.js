be5e56b6ed8b41d00e96218ce342efa118ecd6c0
rainmeo <akimotoming@gmail.com>
2019-09-09 01:00:47 +0800

    Import function is completed but it can't detect ilegal queue yet

--- src/import.js
+++ src/import.js
@@ -1,5 +1,40 @@
 function importOK(){
+    
     console.log("import start")
+    SongClear()
+    
+    const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    
+        String = document.getElementById("comment").value
+
+    ScaleChange(String.substr(0,1))
+        for(j=1;j<=String.length-1;j=j+3){
+ 
+            c1=data.indexOf(String.substr(j+1,1))
+            c2=data.indexOf(String.substr(j+2,1))
+            c3=c1*64+c2
+            
+            if (tab.rows[0].cells.length>c3){
+                
+                tab.rows[1].cells[c3].innerHTML=String.substr(j,1)
+            }else{
+                
+                
+                while(tab.rows[0].cells.length<=c3){
+
+                    NewSongQuery()
+                }
+                    
+                
+                tab.rows[1].cells[c3].innerHTML=String.substr(j,1)
+                
+            }
+            
+    }
+    
+
+
+    OutputString()
     $("#dimScreen").fadeOut() 
 }
 function importC(){
